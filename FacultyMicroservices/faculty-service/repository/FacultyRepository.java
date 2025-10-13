7cb419e528e046b6adfaa3c930e3c01d9c3039bf
Andugala Pujitha <149463713+AndugalaPujitha@users.noreply.github.com>
2025-10-13 12:15:50 +0530

    Delete FacultyMicroservices directory

--- FacultyMicroservices/faculty-service/repository/FacultyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.FacultyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FacultyRepository extends JpaRepository<FacultyEntity, Long> {
-}
