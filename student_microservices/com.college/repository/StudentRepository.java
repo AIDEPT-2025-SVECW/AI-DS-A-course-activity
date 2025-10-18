695f711b548f96de2cf3b51109616b9a3ac89e34
Uma Prasanna <149463701+UmaPrasanna22@users.noreply.github.com>
2025-10-18 11:13:15 +0530

    Delete student_microservices directory

--- student_microservices/com.college/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.college.studentmicroservices.repository;
-
-import com.college.studentmicroservices.entity.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
-}
